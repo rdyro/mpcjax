fixed hessian regularization search

mpcjax/second_order_solvers.py
@@ -1,8 +1,15 @@
+from __future__ import annotations
+
 from enum import Enum
-from typing import NamedTuple
+from typing import NamedTuple, Callable, Any
 from functools import partial
+
 from jfi import jaxm
 from jaxopt import base  # noqa: E402
+import jax
+from jax import numpy as jnp
+
+#from .utils import balanced_solve, f64_solve
 
 
 class LineSearch(Enum):
@@ -15,6 +22,7 @@ class ConvexState(NamedTuple):
     best_params: jaxm.jax.Array
     best_loss: jaxm.jax.Array
     value: jaxm.jax.Array
+    aux: Any | None = None
 
 
 ####################################################################################################
@@ -33,7 +41,10 @@ class ConvexSolver(base.IterativeSolver):
         max_stepsize=1e1,
         reg0=1e-6,
         linesearch="scan",
-        device="cpu",
+        force_step: bool = False,
+        g_fn: Callable | None = None,
+        h_fn: Callable | None = None,
+        has_aux: bool = False,
     ):
         """A jaxopt compatible iterative solver for convex problems only.
 
@@ -54,7 +65,8 @@ class ConvexSolver(base.IterativeSolver):
         self.min_stepsize, self.max_stepsize = min_stepsize, max_stepsize
         self.tol, self.reg0 = tol, reg0
         self.verbose = verbose
-        self.device = device
+        self.force_step = force_step
+        self.has_aux = has_aux
         assert linesearch.lower() in ["scan", "backtracking", "binary_search"]
         if linesearch.lower() == "scan":
             self.linesearch = LineSearch.scan
@@ -65,73 +77,95 @@ class ConvexSolver(base.IterativeSolver):
         else:
             raise ValueError(f"Unknown linesearch: {linesearch}")
 
-        self.f_fn = fun
-
-        def g_fn(params, *args, **kw):
-            g = jaxm.grad(self.f_fn)(params, *args, **kw)
+        def g_fn_constructed(params, *args):
+            if g_fn is not None:
+                g = g_fn(params, *args)
+            else:
+                if self.has_aux:
+                    g = jaxm.grad(lambda params, *args: fun(params, *args)[0])(params, *args)
+                else:
+                    g = jaxm.grad(fun)(params, *args)
             return g.reshape((params.size,))
 
-        def h_fn(params, *args, **kw):
-            H = jaxm.hessian(self.f_fn)(params, *args, **kw)
+        def h_fn_constructed(params, *args):
+            if h_fn is not None:
+                H = h_fn(params, *args)
+            else:
+                if self.has_aux:
+                    H = jaxm.hessian(lambda params, *args: fun(params, *args)[0])(params, *args)
+                else:
+                    H = jaxm.hessian(fun)(params, *args)
             return H.reshape((params.size, params.size))
 
-        self.g_fn, self.h_fn = g_fn, h_fn
-
         if jit:
-            self.f_fn = jaxm.jit(self.f_fn)
-            self.g_fn = jaxm.jit(self.g_fn)
-            self.h_fn = jaxm.jit(self.h_fn)
+            self.f_fn = jaxm.jit(fun)
+            self.g_fn = jaxm.jit(g_fn_constructed)
+            self.h_fn = jaxm.jit(h_fn_constructed)
+        else:
+            self.f_fn = fun
+            self.g_fn = g_fn_constructed
+            self.h_fn = h_fn_constructed
 
-    def init_state(self, params, *args, **kw):
-        best_loss = self.f_fn(params, *args, **kw)
-        return ConvexState(params, best_loss, best_loss)
+    # @partial(jax.jit, static_argnums=(0,), static_argnames=("maxls", "force_step", "linesearch"))
+    def init_state(self, params, *args, **config):
+        if self.has_aux:
+            best_loss, aux = self.f_fn(params, *args)
+            return ConvexState(params, best_loss, best_loss, aux)
+        else:
+            best_loss = self.f_fn(params, *args)
+            return ConvexState(params, best_loss, best_loss)
 
-    def update(self, params, state, *args, **kw):
-        g, H = self.g_fn(params, *args, **kw), self.h_fn(params, *args, **kw)
+    # @partial(jax.jit, static_argnums=(0,), static_argnames=("maxls", "force_step", "linesearch"))
+    def update(self, params, state, *args, **config):
+        g, H = self.g_fn(params, *args), self.h_fn(params, *args)
         cond = jaxm.norm(g) > self.tol
         return jaxm.jax.lax.cond(
-            cond, lambda: self._update(g, H, params, state, *args, **kw), lambda: (params, state)
+            cond,
+            lambda: self._update(g, H, params, state, *args, **config),
+            lambda: (params, state),
         )
 
-    def _update(self, g, H, params, state, *args, **kw):
+    def _update(self, g, H, params, state, *args, **config):
         """Update routine based on the values of the gradient and Hessian."""
         dtype = H.dtype
+        linesearch = config.get("linesearch", self.linesearch)
+        maxls = config.get("maxls", self.maxls)
+        force_step = config.get("force_step", self.force_step)
 
         # compute descent direction
         dp = -jaxm.scipy.linalg.cho_solve(
-            jaxm.scipy.linalg.cho_factor(
-                H + self.reg0 * jaxm.eye(H.shape[-1], dtype=dtype, device=self.device)
-            ),
-            g,
+            jaxm.scipy.linalg.cho_factor(H + self.reg0 * jnp.eye(H.shape[-1], dtype=dtype)), g
         ).reshape(params.shape)
-        if self.linesearch == LineSearch.scan:
-            lower_ls = max(round(float(0.7 * self.maxls)), 1)
-            bets_low = jaxm.to(
-                jaxm.logspace(jaxm.log10(self.min_stepsize), 0.0, lower_ls, dtype=dtype),
-                device=self.device,
-                dtype=dtype,
-            )
-            bets_up = jaxm.to(
-                jaxm.logspace(
-                    0.0,
-                    jaxm.log10(self.max_stepsize),
-                    self.maxls - lower_ls,
-                    dtype=dtype,
-                ),
-                device=self.device,
-                dtype=dtype,
+        # dp = -jaxm.linalg.solve(H + self.reg0 * jnp.eye(H.shape[-1], dtype=dtype), g).reshape(
+        #    params.shape
+        # )
+        # dp = -balanced_solve(H + self.reg0 * jnp.eye(H.shape[-1], dtype=dtype), g[..., None])[
+        #    ..., 0
+        # ].reshape(params.shape)
+        # dp = -f64_solve(H + self.reg0 * jnp.eye(H.shape[-1], dtype=dtype), g[..., None])[
+        #    ..., 0
+        # ].reshape(params.shape)
+        if linesearch == LineSearch.scan:
+            lower_ls = max(round(float(0.7 * maxls)), 1)
+            bets_low = jnp.logspace(jaxm.log10(self.min_stepsize), 0.0, lower_ls, dtype=dtype)
+            bets_up = jnp.logspace(
+                0.0, jaxm.log10(self.max_stepsize), maxls - lower_ls, dtype=dtype
             )[1:]
             bets = jaxm.cat([bets_low, bets_up], -1)
-            losses = jaxm.jax.vmap(lambda bet: self.f_fn(params + bet * dp, *args, **kw))(bets)
+
+            if self.has_aux:
+                losses = jaxm.jax.vmap(lambda bet: self.f_fn(params + bet * dp, *args)[0])(bets)
+            else:
+                losses = jaxm.jax.vmap(lambda bet: self.f_fn(params + bet * dp, *args))(bets)
             losses = jaxm.where(jaxm.isnan(losses), jaxm.inf, losses)
             idx = jaxm.argmin(losses)
             bet, new_loss = bets[idx], losses[idx]
             new_params = params + bet * dp
-        elif self.linesearch == LineSearch.backtracking:
+        elif linesearch == LineSearch.backtracking:
 
             def cond_fn(step):
                 step_not_too_small = step >= self.min_stepsize
-                not_better_loss = self.f_fn(params + step * dp, *args, **kw) > state.best_loss
+                not_better_loss = self.f_fn(params + step * dp, *args) > state.best_loss
                 return jaxm.logical_and(step_not_too_small, not_better_loss)
 
             def body_fn(step):
@@ -139,64 +173,68 @@ class ConvexSolver(base.IterativeSolver):
 
             step_size = jaxm.jax.lax.while_loop(cond_fn, body_fn, 1.0)
             new_params = params + step_size * dp
-            new_loss = self.f_fn(new_params, *args, **kw)
-        elif self.linesearch == LineSearch.binary_search:
+            new_loss = self.f_fn(new_params, *args)
+        elif linesearch == LineSearch.binary_search:
 
             def body_fn(i, val):
                 betl, betr, fl, fr = val
                 # betm = 10.0 ** ((jaxm.log10(betl) + jaxm.log10(betr)) / 2.0)
                 betm = (betl + betr) / 2.0
-                fm = self.f_fn(params + betm * dp, *args, **kw)
+                fm = self.f_fn(params + betm * dp, *args)
                 cond = fl < fr
                 betr, betl = jaxm.where(cond, betm, betr), jaxm.where(cond, betl, betm)
                 fr, fl = jaxm.where(cond, fm, fr), jaxm.where(cond, fl, fm)
                 return (betl, betr, fl, fr)
 
             betl, betr = self.min_stepsize, 1e1
-            fl = self.f_fn(params + betl * dp, *args, **kw)
-            fr = self.f_fn(params + betr * dp, *args, **kw)
+            fl = self.f_fn(params + betl * dp, *args)
+            fr = self.f_fn(params + betr * dp, *args)
             betl, betr, fl, fr = jaxm.jax.lax.fori_loop(
-                0, jaxm.maximum(self.maxls - 2, 1), body_fn, (betl, betr, fl, fr)
+                0, jaxm.maximum(maxls - 2, 1), body_fn, (betl, betr, fl, fr)
             )
             cond = fl < fr
             new_params = jaxm.where(cond, params + betl * dp, params + betr * dp)
             new_loss = jaxm.where(cond, fl, fr)
         else:
-            raise ValueError(f"Unknown line search method {self.linesearch}")
+            raise ValueError(f"Unknown line search method {linesearch}")
 
         best_params = jaxm.where(new_loss <= state.best_loss, new_params, state.best_params)
         best_loss = jaxm.where(new_loss <= state.best_loss, new_loss, state.best_loss)
-        state = ConvexState(best_params, best_loss, best_loss)
-        new_params = state.best_params
+        if self.has_aux:
+            _, aux = self.f_fn(best_params, *args)
+            state = ConvexState(best_params, best_loss, best_loss, aux)
+        else:
+            state = ConvexState(best_params, best_loss, best_loss)
+        if not force_step:
+            new_params = state.best_params
         return new_params, state
 
 
 ####################################################################################################
 cho_factor = jaxm.scipy.linalg.cho_factor
-device = jaxm.jax.devices("cpu")[0]
 
 
-@partial(jaxm.jit, static_argnames=("device", "max_iter"))
-def _find_cholesky_factorization(H, Fl, laml, lamr=1e3, device=device, max_iter=10):
+@partial(jaxm.jit, static_argnames=("max_iter",))
+def _find_cholesky_factorization(H, Fl, laml, max_iter):
     # find the right lambda ###########################
     def cond_fn(lam_F):
-        _, F = lam_F
+        _, _, F = lam_F
         return jaxm.logical_not(jaxm.isfinite(F[0][0, 0]))
 
     def body_fn(lam_F):
-        lam, _ = lam_F
-        lam = lam * 5
-        F = cho_factor(H + lam * jaxm.eye(H.shape[-1], dtype=H.dtype, device=device))
-        return (lam, F)
+        _, lam, _ = lam_F
+        lam, lam_prev = lam * 5, lam
+        F = cho_factor(H + lam * jnp.eye(H.shape[-1], dtype=H.dtype))
+        return (lam_prev, lam, F)
 
-    Fr = cho_factor(H + lamr * jaxm.eye(H.shape[-1], dtype=H.dtype, device=device))
-    lamr, Fr = jaxm.jax.lax.while_loop(cond_fn, body_fn, (lamr, Fr))
+    F = cho_factor(H + laml * jnp.eye(H.shape[-1], dtype=H.dtype))
+    laml, lamr, Fr = jaxm.jax.lax.while_loop(cond_fn, body_fn, (laml, laml, F))
 
     # find the middle lambda via bisection ############
     def body_fn(i, val):
         laml, lamr, Fl, Fr = val
         lamm = (laml + lamr) / 2.0
-        Fm = cho_factor(H + lamm * jaxm.eye(H.shape[-1], dtype=H.dtype, device=device))
+        Fm = cho_factor(H + lamm * jnp.eye(H.shape[-1], dtype=H.dtype))
         cond = jaxm.isfinite(Fm[0][0, 0])
         laml, lamr = jaxm.where(cond, laml, lamm), jaxm.where(cond, lamm, lamr)
         Fl = (jaxm.where(cond, Fl[0], Fm[0]), jaxm.where(cond, Fl[1], Fm[1]))
@@ -204,17 +242,19 @@ def _find_cholesky_factorization(H, Fl, laml, lamr=1e3, device=device, max_iter=
         return (laml, lamr, Fl, Fr)
 
     laml, lamr, Fl, Fr = jaxm.jax.lax.fori_loop(0, max_iter, body_fn, (laml, lamr, Fl, Fr))
+    # print(f"diff = ", lamr - laml)
     return lamr, Fr
 
 
-@partial(jaxm.jit, static_argnames=("device", "max_iter"))
-def positive_cholesky_factorization(H, reg0=0.0, device=device, max_iter=10):
-    F = cho_factor(H + reg0 * jaxm.eye(H.shape[-1], dtype=H.dtype, device=device))
-    return jaxm.jax.lax.cond(
+@partial(jaxm.jit, static_argnames=("max_iter",))
+def positive_cholesky_factorization(H, reg0=0.0, max_iter=10):
+    F = cho_factor(H + reg0 * jnp.eye(H.shape[-1], dtype=H.dtype))
+    ret = jaxm.jax.lax.cond(
         jaxm.isfinite(F[0][0, 0]),
         lambda: (reg0, F),
-        lambda: _find_cholesky_factorization(H, F, reg0, device=device, max_iter=max_iter),
+        lambda: _find_cholesky_factorization(H, F, reg0, max_iter),
     )
+    return ret
 
 
 ####################################################################################################
@@ -233,7 +273,10 @@ class SQPSolver(ConvexSolver):
         max_stepsize=1e1,
         reg0=1e-6,
         linesearch="scan",
-        device="cpu",
+        force_step: bool = False,
+        g_fn: Callable | None = None,
+        h_fn: Callable | None = None,
+        has_aux: bool = False,
     ):
         """A jaxopt compatible iterative solver using sequential quadratic programming.
 
@@ -250,7 +293,7 @@ class SQPSolver(ConvexSolver):
             linesearch (str, optional): Which linesearch to use from
                                     ["scan", "backtracking", "binary_search"]. Defaults to "scan".
         """
-        assert linesearch != "binary_search", "binary_search not supported for non-cvx objectives"
+        assert linesearch != "binary_search", "binary_search not supported for non-convex objetives"
         super().__init__(
             fun,
             maxiter=maxiter,
@@ -262,20 +305,22 @@ class SQPSolver(ConvexSolver):
             max_stepsize=max_stepsize,
             reg0=reg0,
             linesearch=linesearch,
-            device=device,
+            force_step=force_step,
+            g_fn=g_fn,
+            h_fn=h_fn,
+            has_aux=has_aux,
         )
 
-    def update(self, params, state, *args, **kw):
-        g, H = self.g_fn(params, *args, **kw), self.h_fn(params, *args, **kw)
+    @partial(jax.jit, static_argnums=(0,), static_argnames=("maxls", "force_step", "linesearch"))
+    def update(self, params, state, *args, **config):
+        g, H = self.g_fn(params, *args), self.h_fn(params, *args)
         dtype = H.dtype
         cond = jaxm.norm(g) > self.tol
 
-        def find_H_factorization(H):
+        def fix_H_and_update(H):
             # reg = jaxm.minimum(-jaxm.min(jaxm.linalg.eigvalsh(H)), 0.0)
-            reg = positive_cholesky_factorization(H, reg0=self.reg0, device=self.device)[0]
-            H = H + reg * jaxm.eye(H.shape[-1], dtype=dtype, device=self.device)
-            return self._update(g, H, params, state, *args, **kw)
+            reg = positive_cholesky_factorization(H, reg0=self.reg0)[0]
+            H = H + reg * jnp.eye(H.shape[-1], dtype=dtype)
+            return self._update(g, H, params, state, *args, **config)
 
-        return jaxm.jax.lax.cond(
-            cond, lambda H: find_H_factorization(H), lambda H: (params, state), H
-        )
+        return jaxm.jax.lax.cond(cond, lambda H: fix_H_and_update(H), lambda H: (params, state), H)

mpcjax/utils.py
@@ -1,10 +1,15 @@
-from typing import Any
+from __future__ import annotations
+
+from typing import Any, TypeVar
 
 from jfi import jaxm
 from jax.tree_util import tree_map
+from jax import Array, Device
+
+Dtype = TypeVar("Dtype")
 
 
-def _is_numeric(x):
+def _is_numeric(x: Any) -> bool:
     """Check whether and object can be represented as a JAX array."""
     try:
         jaxm.array(x)
@@ -13,16 +18,38 @@ def _is_numeric(x):
         return False
 
 
-def _to_dtype_device(d: Any, device=None, dtype=None):
+def _to_dtype_device(d: Any, device=None, dtype=None) -> Device | Dtype:
     """Convert an arbitrary nested python object to specified dtype and device."""
     return tree_map(
         lambda x: jaxm.to(jaxm.array(x), dtype=dtype, device=device) if _is_numeric(x) else None, d
     )
 
 
-def _jax_sanitize(x: Any) -> Any:
+def _jax_sanitize(x: Any) -> Array | None:
     """Replace all data that cannot be expressed as a JAX array (e.g., str) with None"""
     return tree_map(lambda x: x if _is_numeric(x) else None, x)
+
+
+def balanced_solve(A: Array, y: Array) -> Array:
+    assert A.ndim == y.ndim
+    Pl = 1.0 / jaxm.linalg.norm(A, axis=-1)
+    Pr = 1.0 / jaxm.linalg.norm(A, axis=-2)
+    # Pr = jaxm.ones(A.shape[:-1])
+    Pl = (Pl + Pr) / 2.0
+    Pr = Pl
+    A_norm = Pl[..., None] * A * Pr[..., None, :]
+    # solve_fn = jaxm.linalg.solve
+    solve_fn = lambda M, h: jaxm.scipy.linalg.cho_solve(  # noqa: E731
+        jaxm.scipy.linalg.cho_factor(M), h
+    )
+    return Pr[..., None, :] * solve_fn(A_norm, Pl[..., None] * y)
+
+
+def f64_solve(A: Array, y: Array) -> Array:
+    org_dtype = A.dtype
+    A, y = A.astype(jaxm.float64), y.astype(jaxm.float64)
+    return jaxm.linalg.solve(A, y).astype(org_dtype)
+
 
 ####################################################################################################
 
@@ -31,10 +58,7 @@ class TablePrinter:
     def __init__(self, names, fmts=None, prefix=""):
         self.names = names
         self.fmts = fmts if fmts is not None else ["%9.4e" for _ in names]
-        self.widths = [
-            max(self.calc_width(fmt), len(name)) + 2
-            for (fmt, name) in zip(fmts, names)
-        ]
+        self.widths = [max(self.calc_width(fmt), len(name)) + 2 for (fmt, name) in zip(fmts, names)]
         self.prefix = prefix
 
     def calc_width(self, fmt):
@@ -63,7 +87,7 @@ class TablePrinter:
     def make_header(self):
         s = self.prefix + self.make_row_sep() + "\n"
         s += self.prefix
-        for (name, width) in zip(self.names, self.widths):
+        for name, width in zip(self.names, self.widths):
             s += "|" + self.pad_field("%s" % name, width, lj=True)
         s += "|\n"
         return s + self.prefix + self.make_row_sep()
@@ -74,7 +98,7 @@ class TablePrinter:
     def make_values(self, vals):
         assert len(vals) == len(self.fmts)
         s = self.prefix + ""
-        for (val, fmt, width) in zip(vals, self.fmts, self.widths):
+        for val, fmt, width in zip(vals, self.fmts, self.widths):
             s += "|" + self.pad_field(fmt % val, width, lj=False)
         s += "|"
         return s